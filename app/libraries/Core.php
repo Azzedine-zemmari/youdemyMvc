2269440bb656da704cb608f2895b64647f1546f9
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-02-03 16:49:18 +0100

    add the core file

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -7,7 +7,34 @@ class Core{
 
     public function __construct()
     {
-       print_r( $this->getUrl());
+       $url = $this->getUrl();
+       if(!empty($url)){
+           if(file_exists('../app/controllers/'.$url[0].'.php')){
+               // if exists set as controller
+               $this->curentController = $url[0];
+               // unset the index 0 from the url array
+               unset($url[0]);
+           }
+       }
+       // require the controller
+        require_once "../app/controllers/".$this->curentController.".php";
+
+       // instance the controller
+        $this->curentController = new $this->curentController;
+
+        //check if there's a method
+        if(isset($url[1])){
+            // check if the method exists in the controller
+            if(method_exists($this->curentController,$url[1])){
+                $this->currentMethod = $url[1];
+                // unset the index 1 from the url array
+                unset($url[1]);
+            }
+        }
+        // check the parameter
+        $this->params =$url ? array_values($url) : [];
+
+        call_user_func_array([$this->curentController,$this->currentMethod],$this->params);
     }
 
     public function getUrl(){
