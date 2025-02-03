844baa34cda4b1155e6acd04c78ed15f0587f571
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-02-03 10:27:18 +0100

    start project

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','Azzedine2004');
+define('DB_NAME','...');
+
+// App Root
+define('APPROOT',dirname(dirname(__FILE__)));
+// Url Root
+define('URLROOT','http://localhost/youdemyMvc');
+// site name
+define('SITENAME','YOUDEMY');
